82e39b4e191b6747e312702453042d5bfec458da
Sreenivas K <ksreenivascse@gmail.com>
2020-08-03 21:17:42 +0530

    Operations on Swift

---
+++ Operations.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+print("Operations")
+
+// Operation with similar Types
+// Here all the integers
+var a:Int=1;
+var b:Int=2;
+var c:Int=3;
+var d:Int=4;
+var sum=a+b+c+d
+print(sum)
+
+
+// Operation with different Types
+
+var a2:Int=1;
+var b2:Float=2.1;
+var c2:Double=3.2;
+
+//var sum2:Double=a2+b2+c2; //Binary operator '+' cannot be applied to operands of type 'Int' and 'Float'
+
+var po=pow(2,2)
+var sq=sqrt(4)
+var ce=ceil(5.5) // rounded to Higher value 6.0
+var flo=floor(5.5) // rounded to Lower value 5.0
+
+print(po)
+print(sq)
+print(ce)
+print(flo)
+
