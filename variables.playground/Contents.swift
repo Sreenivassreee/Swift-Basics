035240ae1264a15b7b70d404effa228948a37701
Sreenivas K <ksreenivascse@gmail.com>
2020-08-03 19:28:04 +0530

    variables

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+
+// Variable declaration with Datatype
+
+var name:String="sreenivas k"
+print(name)
+
+// name is the variable name
+// String is the datatype
+
+
+var age:Int=30; // Here the the Int may be optinal so we can write
+var age2 = 25;
+print(age)
+print(age2)
+
+//Data type is optinal
+
+// Variable declaration with Datatype
+
+var firstName="Sree "
+var lastName="Nivas "
+
+print(firstName+lastName)
+
+
+//print() is used to print the Result 
